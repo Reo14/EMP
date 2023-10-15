5fbf414516a9e1d5e45af1f8b8206641657eda13
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-10-14 19:50:35 -0700

    intial theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "./store/configureStore";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <ChakraProvider>
+        <ColorModeScript initialColorMode="light" />
+        <App />
+      </ChakraProvider>
+    </Provider>
+  </React.StrictMode>
+);
