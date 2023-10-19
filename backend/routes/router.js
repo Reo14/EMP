12fe3c52a667bd50a1e7369a818aa87f2494f633
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-10-18 22:26:56 -0700

    corrected many router typos

--- backend/routes/router.js
+++ backend/routes/router.js
@@ -1,27 +1,38 @@
 const express = require("express");
 const router = express.Router();
-const { verifyToken } = require('../middlewares/verifyToken');
-const {signin, signup} = require('../controllers/auth');
+const { verifyToken } = require("../middlewares/verifyToken");
+const { signin, signup } = require("../controllers/auth");
 const {
-    submitOnboardingApplication,
-    getOnboardingApplicationStatus,
-    createPersonalInformation,
-    getPersonalInformation,
-    editPersonalInformation,
-  } = require('../controllers/employeeControllers');
-const { createEmergencyContact, getAllEmergencyContacts, updateEmergencyContact, deleteEmergencyContact } = require('../controllers/emergenceControllers');
-const { createReference, getReference, updateReference } = require('../controllers/referenceControllers');
+  submitOnboardingApplication,
+  getOnboardingApplicationStatus,
+  createPersonalInformation,
+  getPersonalInformation,
+  editPersonalInformation,
+} = require("../controllers/employeeContollers");
 const {
-    getAllEmployeeSummaries,
-    generateRegistrationToken,
-    getRegistrationTokenHistory,
-    processOnboardingApplication,
-    getAllOnboardingApplications,
-    getOnboardingApplicationsByStatus
-  } = require('../controllers/hrControllers')
+  createEmergencyContact,
+  getAllEmergencyContacts,
+  updateEmergencyContact,
+  deleteEmergencyContact,
+} = require("../controllers/emergenceControllers");
+const {
+  createReference,
+  getReference,
+  updateReference,
+} = require("../controllers/referenceControllers");
+const {
+  getAllEmployeeSummaries,
+  generateRegistrationToken,
+  getRegistrationTokenHistory,
+  processOnboardingApplication,
+  getAllOnboardingApplications,
+  getOnboardingApplicationsByStatus,
+} = require("../controllers/hrControllers");
+const { checkExistence } = require("../utils/query");
+// 请根据前端的实际需要修改此处的router
 
-// 请根据前端的实际需要修改此处的router 
-
+// Query
+router.get("/query", checkExistence);
 
 // Auth Flow
 router.post("/sign-up", verifyToken, signup);
@@ -30,29 +41,29 @@ router.post("/sign-in", signin);
 // router.post("/update-pwd", updatePassword);
 
 // Employee Flow
-router.post("submit-onboardingapplication", submitOnboardingApplication );
-router.get("status/:id", getOnboardingApplicationStatus)
-router.get("personal-information/:id", getPersonalInformation);
-router.put("personal-information/:id/edit", editPersonalInformation);
-router.post("personal-information/:id/create", createPersonalInformation);
+router.post("/submit-onboardingapplication", submitOnboardingApplication);
+router.get("/status/:id", getOnboardingApplicationStatus);
+router.get("/personal-information/:id", getPersonalInformation);
+router.put("/personal-information/:id/edit", editPersonalInformation);
+router.post("/personal-information/:id/create", createPersonalInformation);
 
 // Emergence Contact Flow
-router.get("emergence-contact/:id", getAllEmergencyContacts);
-router.post("emergence-contact/:id/create", createEmergencyContact);
-router.put("emergence-contact/:id/edit", updateEmergencyContact);
-router.delete("emergence-contact/:id/delete", deleteEmergencyContact);
+router.get("/emergence-contact/:id", getAllEmergencyContacts);
+router.post("/emergence-contact/:id/create", createEmergencyContact);
+router.put("/emergence-contact/:id/edit", updateEmergencyContact);
+router.delete("/emergence-contact/:id/delete", deleteEmergencyContact);
 
 // Reference Flow
-router.get("reference/:id", getReference);
-router.post("reference/:id/create", createReference);
-router.put("reference/:id/edit", updateReference);
+router.get("/reference/:id", getReference);
+router.post("/reference/:id/create", createReference);
+router.put("/reference/:id/edit", updateReference);
 // 由于reference的数量只能为1 我没有设置delete函数
 
 // HR Flow
-router.get("hr/all-employees", getAllEmployeeSummaries);
-router.post("hr/registration/send", generateRegistrationToken);
-router.get("hr/registration/history", getRegistrationTokenHistory);
-router.get("hr/onboardapplication", getAllOnboardingApplications)
-router.put("hr/onboardapplication/process", processOnboardingApplication)
+router.get("/hr/all-employees", getAllEmployeeSummaries);
+router.post("/hr/registration/send", generateRegistrationToken);
+router.get("/hr/registration/history", getRegistrationTokenHistory);
+router.get("/hr/onboardapplication", getAllOnboardingApplications);
+router.put("/hr/onboardapplication/process", processOnboardingApplication);
 
 module.exports = router;
