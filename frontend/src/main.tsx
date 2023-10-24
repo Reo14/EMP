ca1c24b57260f2940f146649e7b8f0a74e73deff
Lu0-Wenfeng <vincent_lwf@hotmail.com>
2023-10-23 21:32:34 -0700

    added redux-persist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,15 +3,18 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import { Provider } from "react-redux";
-import { store } from "./store/configureStore";
+import { store, persistor } from "./store/configureStore";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ChakraProvider>
-        <ColorModeScript initialColorMode="light" />
-        <App />
-      </ChakraProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ChakraProvider>
+          <ColorModeScript initialColorMode="light" />
+          <App />
+        </ChakraProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
